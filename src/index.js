b0ee106aeb6a14d1963f0e16ca99a1b6f427675e
Luci <deepanjansaha100@gmail.com>
2025-10-16 18:27:17 +0530

    maintenance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const BOT_ACTIVE = process.env.BOT_ACTIVE === "true";
+const BOT_ACTIVE = process.env.BOT_ACTIVE === "false";
 const fs = require("fs");
 const path = require("path");
 const { Client, Collection, IntentsBitField, REST, Routes } = require("discord.js");
