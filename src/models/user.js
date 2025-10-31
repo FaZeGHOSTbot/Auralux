4351b663d628f52e52f368c99a78ffa4d971f1b9
Luci <deepanjansaha100@gmail.com>
2025-11-01 04:26:55 +0530

    potential not saving in mongodb fix

--- src/models/user.js
+++ src/models/user.js
@@ -34,7 +34,7 @@
 
     const userSchema = new mongoose.Schema({
       userId: { type: String, required: true },
-      guildId: { type: String, required: true },
+      guildId: { type: String, required: false },
 
       xp: { type: Number, default: 0 },
       level: { type: Number, default: 1 },
